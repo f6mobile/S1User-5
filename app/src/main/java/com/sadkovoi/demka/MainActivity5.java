94ad8ccd8e04ded71a1aa992e48c2a39b57ed95a
user <ggewew@gmail.com>
2022-06-16 18:53:54 +0300

    Initial commit

---
+++ app/src/main/java/com/sadkovoi/demka/MainActivity5.java
@@ -0,0 +1,20 @@
+package com.sadkovoi.demka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+
+    public void enter(View view) {
+    }
+
+
+    }
